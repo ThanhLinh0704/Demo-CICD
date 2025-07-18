1aec1b8cf2c8ee62ec9363f24c198ad069c7f096
ThanhLinh0704 <lethanhlinh0704@gmail.com>
2025-07-19 00:16:12 +0900

    first commit

---
+++ src/main/java/linhlt/fpt/demo/Controller/HelloController.java
@@ -0,0 +1,12 @@
+package linhlt.fpt.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
